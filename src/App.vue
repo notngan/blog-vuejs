e24ecf7eef6c940daddc9b8e6e4231a907f5f7fa
notngan <notngan@gmail.com>
2018-08-16 04:19:09 +0700

    vue-router

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+		<router-view></router-view>		
+	</div>
+</template>
+
+<script>
+// Imports
+import addBlog from './components/addBlog.vue';
+import showBlog from './components/showBlog.vue';
+import listBlog from './components/listBlog.vue';
+
+export default {
+    components: {
+      'add-blog': addBlog,
+			'show-blog': showBlog,
+			'list-blog': listBlog
+    },
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito');
+
+body{
+  margin: 0;
+	font-family: 'Nunito', sans-serif;
+	}
+</style>
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import showBlog from './components/showBlog.vue'
+import addBlog from './components/addBlog.vue'
+
+export default [
+	{ path: '/', component: showBlog },
+	{ path: '/add', component: addBlog }
+]
